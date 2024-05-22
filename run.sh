396715a8e060f81360c148f4bcc23ac0357cf72a
ceald1 <fergusonliam04@gmail.com>
2024-05-21 20:04:52 -0700

    small fixes

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo "starting and building containers...."
 docker compose up --build -d
 
 
-echo "done!, run: `docker compose down -v` to stop containers and delete volumes"
+echo "done!, run: 'docker compose down -v' to stop containers and delete volumes"
